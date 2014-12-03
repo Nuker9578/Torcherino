a191be8c76909f24e24601a400201872b2777a97
sci4me <minecraftman7778@gmail.com>
2014-12-02 20:24:17 -0600

    cool stuff

--- src/main/java/com/sci/torcherino/Torcherino.java
+++ src/main/java/com/sci/torcherino/Torcherino.java
@@ -8,43 +8,53 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
 
 /**
  * @author sci4me
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
 @Mod(modid = Props.ID, name = Props.NAME, version = Props.VERSION, dependencies = Props.DEPENDENCIES)
-public class Torcherino implements IUpdatableMod
-{
+public class Torcherino implements IUpdatableMod {
     private static Torcherino instance;
 
     @Mod.InstanceFactory
-    public static Torcherino instance()
-    {
+    public static Torcherino instance() {
         if (Torcherino.instance == null)
             Torcherino.instance = new Torcherino();
         return Torcherino.instance;
     }
 
-    private Torcherino()
-    {
+    private Torcherino() {
     }
 
     @Mod.EventHandler
-    public void preInit(final FMLPreInitializationEvent evt)
-    {
-        ModBlocks.init();
-        Recipes.init();
+    public void preInit(final FMLPreInitializationEvent evt) {
+        final File folder = new File(evt.getModConfigurationDirectory(), "sci4me");
+
+        if (!folder.exists())
+            folder.mkdir();
+
+        final Configuration cfg = new Configuration(new File(folder, "Torcherino.cfg"));
+        try {
+            cfg.load();
+
+            ModBlocks.init();
+            Recipes.init(cfg);
+        } finally {
+            if (cfg.hasChanged())
+                cfg.save();
+        }
     }
 
     @Mod.EventHandler
-    public void init(final FMLInitializationEvent evt)
-    {
+    public void init(final FMLInitializationEvent evt) {
     }
 
     @Mod.EventHandler
-    public void postInit(final FMLPostInitializationEvent evt)
-    {
+    public void postInit(final FMLPostInitializationEvent evt) {
     }
 
     @Override
