e4b121a66ebd532172e82e6dfe6341247e15dc1b
sci4me <minecraftman7778@gmail.com>
2014-12-06 14:32:26 -0600

    wow

--- src/main/java/com/sci/torcherino/update/ModVersion.java
+++ src/main/java/com/sci/torcherino/update/ModVersion.java
@@ -1,83 +1,43 @@
 package com.sci.torcherino.update;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Maps;
-
-import java.util.Map;
-
 /**
  * @author sci4me
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
-public final class ModVersion
-{
-    private static final ModVersion NULL_VERSION = new ModVersion(0, 0, 0, 0);
+public final class ModVersion {
+    private static final ModVersion NULL_VERSION = new ModVersion(0, 0);
 
-    private int mc;
     private int major;
     private int minor;
-    private int patch;
 
-    public ModVersion(final int mc, final int major, final int minor, final int patch)
-    {
-        this.mc = mc;
+    public ModVersion(final int major, final int minor) {
         this.major = major;
         this.minor = minor;
-        this.patch = patch;
     }
 
-    public static ModVersion parse(final String version)
-    {
-        if (version.equals("@VERSION@"))
-            return NULL_VERSION;
-
-        try
-        {
+    public static ModVersion parse(final String version) {
+        try {
             final String[] parts = version.split("\\.");
 
             return new ModVersion(
                     Integer.valueOf(parts[0]),
-                    Integer.valueOf(parts[1]),
-                    Integer.valueOf(parts[2]),
-                    Integer.valueOf(parts[3])
+                    Integer.valueOf(parts[1].substring(0, parts[1].length() - 1))
             );
-        }
-        catch (Throwable t)
-        {
-            return null;
+        } catch (Throwable t) {
+            return NULL_VERSION;
         }
     }
 
     @Override
-    public String toString()
-    {
-        return this.mc + "." + this.major + "." + this.minor + "." + this.patch;
+    public String toString() {
+        return this.major + "." + this.minor;
     }
 
-    public boolean isNewer(final ModVersion newer)
-    {
-        if (newer.mc > this.mc)
-        {
+    public boolean isNewer(final ModVersion newer) {
+        if (newer.major > this.major) {
             return true;
-        }
-        else
-        {
-            if (newer.major > this.major)
-            {
-                return true;
-            }
-            else
-            {
-                if (newer.minor > this.minor)
-                {
-                    return true;
-                }
-                else
-                {
-                    if (newer.patch > this.patch)
-                        return true;
-                }
-            }
+        } else if (newer.minor > this.minor) {
+            return true;
         }
         return false;
     }
